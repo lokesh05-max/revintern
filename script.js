71eb46d7b855520b3c39028932c6a6e9450fa12c
lokesh05-max <lokeshgm05@gmail.com>
2025-11-13 14:25:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 const addButton = document.getElementById("addButton");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
-
-
 addButton.addEventListener("click", function() {
   const taskText = taskInput.value.trim();
   if (taskText === "") return;
-
-  
   const li = document.createElement("li");
-
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-
   const span = document.createElement("span");
   span.textContent = " " + taskText;
-
   checkbox.addEventListener("change", function() {
     if (checkbox.checked) {
       span.style.textDecoration = "line-through";
@@ -23,23 +16,20 @@ addButton.addEventListener("click", function() {
       span.style.textDecoration = "none";
     }
   });
-
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
   deleteButton.addEventListener("click", function() {
     taskList.removeChild(li);
   });
-
   li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(deleteButton);
   taskList.appendChild(li);
-
   taskInput.value = "";
 });
-
 taskInput.addEventListener("keypress", function(e) {
   if (e.key === "Enter") {
     addButton.click();
   }
 });
+
